fef77499b18d74064eb301eda34ff65942c684bb
RobertCalder1 <robertcalder27@hotmail.com>
2022-01-24 14:29:23 +1100

    create init function

--- index.js
+++ index.js
@@ -57,7 +57,15 @@ const questions = [
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer
+    .prompt(questions)
+    .then((userInput) =>
+      writeToFile("./readme-folder/README.md", generateMarkdown(userInput))
+    )
+    .then(() => console.log("README successfully created"))
+    .catch((err) => console.error(err));
+}
 
 // Function call to initialize app
 init();
