049fc02223e1ea96be57f588a34ddbad68d43cd5
RobertCalder1 <robertcalder27@hotmail.com>
2022-01-24 14:25:05 +1100

    add license badge and link

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,54 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(licenseProject) {
+  let badge;
+  switch (licenseProject) {
+    case "MIT":
+      badge =
+        "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)";
+      break;
+    case "APACHE 2.0":
+      badge =
+        "![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)";
+      break;
+    case "GPL 3.0":
+      badge =
+        "![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)";
+      break;
+    case "BSD 3":
+      badge =
+        "![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)";
+      break;
+    case "None":
+      badge = "";
+      break;
+  }
+  return badge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let licenseLink;
+  switch (licenseProject) {
+    case "MIT":
+      licenseLink = "https://opensource.org/licenses/MIT";
+      break;
+    case "APACHE 2.0":
+      licenseLink = "https://opensource.org/licenses/Apache-2.0";
+      break;
+    case "GPL 3.0":
+      licenseLink = "https://www.gnu.org/licenses/gpl-3.0";
+      break;
+    case "BSD 3":
+      licenseLink = "https://opensource.org/licenses/BSD-3-Clause";
+      break;
+    case "None":
+      licenseLink = "";
+      break;
+  }
+  return licenseLink;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
